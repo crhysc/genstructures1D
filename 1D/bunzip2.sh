89203133e2d5e1c9d28307441003e196265b34ab
Campbell <crc00042@mix.wvu.edu>
2025-06-04 14:31:01 -0600

    remove scripts that were moved

--- 1D/bunzip2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-mkdir -p interim
-cd interim
-bunzip2 ../raw/*
-mv ../raw/* .
--- 1D/preprocess_agpt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-mkdir -p processed_agpt
-pip install -r data_reqs.txt
-python generate_id_prop.py \
-    -i interim
-    -o processed_agpt
